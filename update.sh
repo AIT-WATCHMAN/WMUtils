4b3cb93a7a403240cfd76157581db0656db203e0
Morgan Askins <maskins@ucdavis.edu>
2019-07-15 17:05:13 -0700

    Some unsafe git hooks =D

--- update.sh
+++ update.sh
@@ -1,1 +1,11 @@
+
+if ! [ -f "./.git/hooks/post-checkout" ]
+then
+  cp .additions/post-checkout ./.git/hooks/post-checkout
+fi
+if ! [ -f "./.git/hooks/post-merge" ]
+then
+  cp .additions/post-merge ./.git/hooks/post-merge
+fi
+
 git submodule update --recursive --init
